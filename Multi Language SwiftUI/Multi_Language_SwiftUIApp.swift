e67aa0f71d3fe579876068525c90e18b5a6cd647
YILMAZ ER <developer.yilmazer@gmail.com>
2023-05-20 05:13:03 +0300

    Initial Commit

---
+++ Multi Language SwiftUI/Multi_Language_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multi_Language_SwiftUIApp.swift
+//  Multi Language SwiftUI
+//
+//  Created by YILMAZ ER on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Multi_Language_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
